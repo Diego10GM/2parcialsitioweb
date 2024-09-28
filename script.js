1d63a4fe9fb0295359f4668d6bce8c92b4f1b039
Diego10GM <diegofranciscog13@gmail.com>
2024-09-28 12:39:58 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+console.log("¡Bienvenido a nuestro sirio web de ventas de ganado bovino y maquinaria agrícola!");
+
+window.onload = function() {
+    alert("Bienvenido a nuestro sitio web de productos ganaderos y agrícolas.");
+};
